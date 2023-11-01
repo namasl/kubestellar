0b04cf33d80941d69be79808e53f930f2ce68d89
Nick Masluk <nick@randombytes.net>
2023-11-01 17:23:23 -0400

    updates

--- cmd/kubectl-kubestellar/cmd/remove/location.go
+++ cmd/kubectl-kubestellar/cmd/remove/location.go
@@ -17,41 +17,98 @@ package remove
 
 import (
     "fmt"
+    "context"
 
     "github.com/spf13/cobra"
+    "github.com/spf13/pflag"
 
 	"k8s.io/cli-runtime/pkg/genericclioptions"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/klog/v2"
+
+    kcpclientset "github.com/kcp-dev/kcp/pkg/client/clientset/versioned"
+
+	clientopts "github.com/kubestellar/kubestellar/pkg/client-options"
 )
 
-var cmdLocation = &cobra.Command{
-    Use:   "location",
-    Aliases: []string{"loc"},
-    Short:  "Delete an inventory entry for a given WEC",
-    Args:  cobra.ExactArgs(1),
-    Run: func(cmd *cobra.Command, args []string) {
-        fmt.Printf("REMOVE LOCATION %s\n", args[0])
-    },
-}
+var imw string // IMW workspace path
 
 // Create the Cobra sub-command for 'kubectl kubestellar remove location'
 func newCmdRemoveLocation(cliOpts *genericclioptions.ConfigFlags) *cobra.Command {
-    // Make wmw command
+    // Make location command
     cmdLocation := &cobra.Command{
-        Use:   "location <LOCATION_NAME>",
+        Use:   "location --imw <IMW_NAME> <LOCATION_NAME>",
         Aliases: []string{"loc"},
         Short:  "Delete an inventory entry for a given WEC",
         Args:  cobra.ExactArgs(1),
         RunE: func(cmd *cobra.Command, args []string) error {
+            // At this point set silence usage to true, so that any errors
+            // following do not result in the help being printed. We only
+            // want the help to be displayed when the error is due to an
+            // invalid command.
+            cmd.SilenceUsage = true
             err := removeLocation(cmd, cliOpts, args)
             return err
         },
     }
 
+    // add flag for IMW workspace
+
+    cmdLocation.Flags().StringVar(&imw, "imw", "", "IMW workspace")
+    cmdLocation.MarkFlagRequired("imw")
     return cmdLocation
 }
 
 // Perform the actual location removal
-func removeLocation(wmwCmd *cobra.Command, cliOpts *genericclioptions.ConfigFlags, args []string) error {
-    fmt.Printf("REMOVE LOC %s\n", args[0])
+func removeLocation(cmdLocation *cobra.Command, cliOpts *genericclioptions.ConfigFlags, args []string) error {
+    locationName := args[0]
+    ctx := context.Background()
+	logger := klog.FromContext(ctx)
+
+    fmt.Printf("REMOVE LOC %s, IMW=%s\n", locationName, imw)
+
+    // Print all flags and their values if verbosity level is at least 1
+	cmdLocation.Flags().VisitAll(func(flg *pflag.Flag) {
+		logger.V(1).Info(fmt.Sprintf("Command line flag %s=%s", flg.Name, flg.Value))
+	})
+
+    // Options for IMW workspace
+	imwClientOpts := clientopts.NewClientOpts(imw, "access to the IMW workspace")
+	// set default context to "workspace.kcp.io/current"
+	imwClientOpts.SetDefaultCurrentContext("workspace.kcp.io/current")
+	// Make a new flag set named rmloc
+	fs := pflag.NewFlagSet("rmloc", pflag.ExitOnError)
+	// Add cliOpts flags to fs (flow from syntax is confusing, goes -->)
+	cliOpts.AddFlags(fs)
+    // add fs to imwClientOpts
+    imwClientOpts.AddFlags(fs)
+
+    // Get client config from flags
+    config, err := imwClientOpts.ToRESTConfig()
+	if err != nil {
+		logger.Error(err, "Failed to get config from flags")
+		return err
+	}
+
+    // Create client-go instance from config
+    client, err := kcpclientset.NewForConfig(config)
+	if err != nil {
+		logger.Error(err, "Failed create client-go instance")
+		return err
+	}
+
+    fmt.Println("****** GET ******")
+    get, err := client.TenancyV1alpha1().Workspaces().Get(ctx, imw, metav1.GetOptions{})
+	if err != nil {
+		logger.Error(err, "Failed to get workspace")
+		return err
+	}
+    fmt.Println(get)
+
+
+    //kubectl "${kubectl_flags[@]}" delete synctargets.edge.kubestellar.io "$objname"
+
+    //kubectl "${kubectl_flags[@]}" delete locations.edge.kubestellar.io "$objname"
+
     return nil
 }
